bbf84559e10984c78f4d14776e122fef8b97e840
Elena Ter-Akopova <testfrog@yandex.ru>
2023-10-03 16:46:40 +0300

    Рефакторинг кода

--- tests/test_get_list_users.py
+++ tests/test_get_list_users.py
@@ -5,22 +5,21 @@ from conftest import load_json_schema
 
 
 def test_successful_get_list_users_by_page():
-    page = 3
-    response = requests.get('https://reqres.in/api/users', params={'page': page})
+    params = {'page': 3}
+    response = requests.get('https://reqres.in/api/users', params=params)
 
     assert response.status_code == 200
-    assert response.json()['page'] == page
+    assert response.json()['page'], [1] == params
 
 
 def test_successful_get_list_users_data_by_per_page():
-    per_page = 5
+    params = {'per_page': 5}
 
-    response = requests.get('https://reqres.in/api/users',
-                            params={'per_page': per_page})
+    response = requests.get('https://reqres.in/api/users', params=params)
 
     assert response.status_code == 200
-    assert response.json()['per_page'] == per_page
-    assert len(response.json()['data']) == per_page
+    assert response.json()['per_page'], [1] == params
+    assert len(response.json()['data']), [1] == params
 
 
 def test_get_list_users_schema_response_format():
